b647c22d356a2c0b6ad0dcef1878f6609aa88ef6
VampireSH <sherlockya@gmail.com>
2016-12-29 12:02:19 +0800

    自定义函数

--- 函数/myabs.py
+++ 函数/myabs.py
@@ -1,4 +1,6 @@
 def my_abs(x):        # 定义一个函数叫做my_abs
+    if not isinstance(x,(int,float)):        # 对输入参数进行类型检查
+        raise TypeError('参数错误')
     if x >= 0:        # 从这里开始到下面是函数的主体
         return x
     else:
