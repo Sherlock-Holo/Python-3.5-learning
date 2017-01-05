0ef74e6dbe449cfd987367e16ad6bc5bc3c30cd7
VampireSH <sherlockya@gmail.com>
2017-01-06 00:22:09 +0800

    将不规范的大小写转换为小写

--- 函数/normallize.py
+++ 函数/normallize.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
-def normallize(bad_str):
+def normallize(bad_str):        # 函数将不规范的大小写转换为全小写
     
-    def bad2normal(n):
+    def bad2normal(n):        # 调取.lower()来转换
         return n.lower()
 
-    return list(map(bad2normal,bad_str))
+    return list(map(bad2normal,bad_str))        # 转换
