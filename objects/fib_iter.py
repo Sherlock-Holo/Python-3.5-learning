0726879f789a4861a14e815ed86daa96f6eeb754
VampireSH <sherlockya@gmail.com>
2017-02-06 18:52:34 +0800

    fib_iter

--- objects/fib_iter.py
+++ objects/fib_iter.py
@@ -12,3 +12,10 @@ class fib(object):
         if self.a > 100000:
             raise StopIteration()
         return self.a
+
+    def __getitem__(self,n):
+        self.a = 1
+        self.b = 1
+        for x in range(n):
+            self.a,self.b = self.b,self.a + self.b
+        return self.a
