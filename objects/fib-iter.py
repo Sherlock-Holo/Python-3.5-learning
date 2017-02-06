901f09d7860f05733df71cbce27590bd33080387
VampireSH <sherlockya@gmail.com>
2017-02-06 16:11:35 +0800

    fib-iter

---
+++ objects/fib-iter.py
@@ -0,0 +1,19 @@
+class fib(object):
+
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a = self.b
+        self.b = self.a + self.b
+        if self.a > 100000:
+            raise StopIteration()
+        return self.a
+
+
+for n in fib():
+    print(n)
