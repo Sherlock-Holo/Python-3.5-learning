33e73d0140eae5d436661e1fb716f80896076e2e
VampireSH <sherlockya@gmail.com>
2017-02-07 22:27:31 +0800

    make it more beautiful

--- objects/fib_iter.py
+++ objects/fib_iter.py
@@ -8,14 +8,14 @@ class fib(object):
         return self
 
     def __next__(self):
-        self.a,self.b = self.b, self.a + self.b
+        self.a, self.b = self.b, self.a + self.b
         if self.a > 100000:
             raise StopIteration()
         return self.a
 
-    def __getitem__(self,n):
+    def __getitem__(self, n):
         self.a = 1
         self.b = 1
         for x in range(n):
-            self.a,self.b = self.b,self.a + self.b
+            self.a, self.b = self.b, self.a + self.b
         return self.a
